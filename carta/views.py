1d4099888ac3005983143ba17113058b221dd22c
Luis Ospina <lfog1012@gmail.com>
2024-08-25 23:59:03 -0500

    versión 1.0 terminada, cartas a partir de un modelo de base de datos y ubicaciones con un mapa apartir de un framework

--- carta/views.py
+++ carta/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from .models import Product
 
 # Create your views here.
 # request -> response
-
+# hacer llamdos a la base de datos
+# enviarles emails a los clientes
 def home(request):
     return render(request, "home.html")
 
 def carta(request):
-    return render(request, "carta.html")
+
+    productos = Product.objects.all()
+    return render(request, "carta.html", {"productos": productos})
 
 def ubicacaciones(request):
     return render(request, "ubicaciones.html")
